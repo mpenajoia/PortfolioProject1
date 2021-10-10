c23e099057ec44a2530205107104b7f42d92add4
Marco Penajoia <penajoia@MPenajoias-MBP.local>
2021-10-09 17:27:55 -0700

    Scrapped jQuery animation for CSS animation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,82 @@
-function down() {
-    $('#arrow').animate({
+
+/*
+//Functions for animated arrows
+//function animating the down arrow to move up 
+function downArrowUp() {
+    $('#arrow-down').animate({
         bottom: '+=10px'
-    }, 400, up)
+    }, 400, downArrowDown)
 }
-
-function up() {
-    $('#arrow').animate({
+//function animating the down arrow to move down
+function downArrowDown() {
+    $('#arrow-down').animate({
         bottom: '-=10px'
-    }, 400, down)
+    }, 400, downArrowUp)
 }
-function bounce() {
-    down();
-    up();
+//function to trigger both functions above
+function downArrowBounce() {
+    downArrowDown();
+    downArrowUp();
+}
+//envoking above functions
+downArrowBounce();
+*/
+
+
+// function upArrowAppear() {
+//     $('#arrow-up').css({'display': 'block'})
+// };
+// upArrowAppear().on('scroll')
+
+
+
+
+/*
+// mirrored functions from above with opposite settings for the up arrow
+function upArrowDown() {
+    $('#arrow-up').animate({
+        bottom: '+=10px'
+    }, 400, upArrowUp)
 }
 
-bounce();
+function upArrowUp() {
+    $('#arrow-up').animate({
+        bottom: '-=10px'
+    }, 400, upArrowDown)
+}
+function upArrowBounce() {
+    upArrowDown();
+    upArrowUp();
+}
+
+upArrowBounce();
+*/
+
+// $(window).scroll(function(){
+//     $('#arrow-up').fadeIn('slow', function() {}).css({'opacity': .1})
+// })
+// .css({'opacity': 1})
+
+/*
+$(window).scroll(function(){
+    const scrollTop = $(window).scrollTop();
+    if(scrollTop > 200){
+        $('#arrow-up').fadeIn(4000, function(){
+            $('#arrow-up').css({'opacity': .1})
+        });
+    }
+})
+*/
+
+// const $scrollTop = $(window).scrollTop();
+// if($scrollTop > 500 && $scrollTop < 900){
+//     $('#arrow-up').fadeIn(400)
+// }else{
+//     $('#arrow-up').fadeOut(400)
+// }
+
+
+
+
+
+
